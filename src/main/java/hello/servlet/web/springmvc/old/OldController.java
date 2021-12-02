386ec6d30ec2bae2c984a50341bf7bc0d830f838
son jin yeong <handsun000@gmail.com>
2021-12-03 08:33:08 +0900

    OldController

--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -12,7 +12,7 @@ public class OldController implements Controller {
 
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        System.out.println("OldController.handleRequest = OldController.handleRequest");
+        System.out.println("OldController.handleRequest");
         return new ModelAndView("new-form");
     }
 }
